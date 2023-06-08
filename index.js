d5fc2b6017a13567025338ee741762eac24f58ca
iabdulkader <abdulkadernow@gmail.com>
2023-06-09 01:43:15 +0600

    test

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const corsProxy = require("cors-anywhere");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const host = process.env.HOST; // Replace with your desired hostname
-const port = 8080; // Replace with your desired port number
+const host = process.env.HOST || "0.0.0.0"; // Replace with your desired hostname
+const port = process.env.PORT || 8080; // Replace with your desired port number
 
 const server = corsProxy.createServer({
   originWhitelist: [], // Allow all origins
